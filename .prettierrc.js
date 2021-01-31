8439c58b1f54681e1ac4c21c4d95543a6484b57f
Baek2back <dumbuk12@naver.com>
2021-01-31 17:21:44 +0900

    feat: eslint,prettier and settings.json for vscode

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'none',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  embeddedLanguageFormatting: 'auto'
+};
